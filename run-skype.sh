90b5413018144445e42d1b0e25e14f8179233a71
Martin Peters <martin@freakybytes.net>
2016-01-31 16:53:25 +0200

    fixed container name

--- run-skype.sh
+++ run-skype.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -d -p 127.0.0.1:55757:22 --privileged -v /dev/video0:/dev/video0 --name skype-container docker-skype
+docker run -d -p 127.0.0.1:55757:22 --privileged -v /dev/video0:/dev/video0 --name skype-container docker-skype && \
+  sleep 1 && \
+  ssh -X -p 55757 -R 64713:localhost:4713 -i ~/.ssh/skype-on-docker docker@127.0.0.1 && \
+  docker rm -f skype-container
