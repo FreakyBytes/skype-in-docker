78f74b1b2b483141d4a9b02d59f1c646df55d5bd
Martin Peters <martin@freakybytes.net>
2017-04-24 23:28:41 +0200

    fixed run script

--- run-skype.sh
+++ run-skype.sh
@@ -1,5 +1,25 @@
-#!/bin/sh
-docker run -d --rm -p 127.0.0.1:55757:22 --privileged -v /dev/video0:/dev/video0 --name skype-container pulse-skype:latest && \
-  sleep 1 && \
-  ssh -X -p 55757 -R 64713:localhost:4713 -i ~/.ssh/skype-on-docker docker@127.0.0.1 && \
-  docker stop skype-container
+#!/bin/bash
+
+# function to stop/remove container
+function container-stop {
+	docker stop skype-container
+}
+
+SSH_KEY_NAME="skype-container"
+
+# create keys dir
+mkdir keys || true
+
+if [ ! -f keys/${SSH_KEY_NAME} ]
+then
+	# generate new keys, since they do not exists
+	(cd keys && ssh-keygen -b 4096 -N "" -f $SSH_KEY_NAME)
+fi
+
+# start container
+docker run -d --rm -p 127.0.0.1:55757:22 --privileged -v /dev/video0:/dev/video0 -e "SSHD_AUTHORIZED_KEYS=$(cat keys/${SSH_KEY_NAME}.pub)" --name skype-container pulse-skype:latest || exit 1
+
+# sleep a while aka. wait until container is up and running
+sleep 1
+ssh -X -p 55757 -R 4713:localhost:4713 -i keys/${SSH_KEY_NAME} docker@127.0.0.1 || container-stop
+container-stop
